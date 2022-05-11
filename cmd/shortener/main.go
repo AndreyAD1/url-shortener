7b3e47196f3c5b862c445944a0a1b94a67a0d60a
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2022-05-11 21:41:24 +0300

    add the console arguments

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/caarlos0/env/v6"
@@ -9,10 +10,32 @@ import (
 	"github.com/AndreyAD1/url-shortener/internal/app/server"
 )
 
+var (
+	serverAddress   *string
+	baseURL         *string
+	fileStoragePath *string
+)
+
+func init() {
+	serverAddress = flag.String("a", "", "a server address")
+	baseURL = flag.String("b", "", "a shorten URL host")
+	fileStoragePath = flag.String("f", "", "a path to a file storage")
+}
+
 func main() {
 	cfg := config.StartupConfig{}
 	if err := env.Parse(&cfg); err != nil {
 		log.Fatalf("%+v\n", err)
+	}
+	flag.Parse()
+	if *serverAddress != "" {
+		cfg.ServerAddress = *serverAddress
+	}
+	if *baseURL != "" {
+		cfg.BaseURL = *baseURL
+	}
+	if *fileStoragePath != "" {
+		cfg.FileStoragePath = *fileStoragePath
 	}
 	srv := server.NewServer(cfg)
 	err := srv.ListenAndServe()
