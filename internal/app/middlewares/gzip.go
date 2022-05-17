0761773c17aab21689593aa460e9a45523ab8e31
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2022-05-17 19:53:53 +0300

    delete the redundant code

--- internal/app/middlewares/gzip.go
+++ internal/app/middlewares/gzip.go
@@ -1,7 +1,6 @@
 package middlewares
 
 import (
-	"bytes"
 	"compress/gzip"
 	"io"
 	"net/http"
@@ -11,26 +10,10 @@ import (
 type gzipWriter struct {
 	http.ResponseWriter
 	ZipWriter    io.Writer
-	CommonWriter http.ResponseWriter
-}
-
-var compressedContentTypes = []string{
-	"application/javascript",
-	"application/json",
-	"text/css",
-	"text/html",
-	"text/plain",
-	"text/xml",
 }
 
 func (w gzipWriter) Write(b []byte) (int, error) {
-	currentContentType := w.Header().Get("Content-Type")
-	for _, compressedContentType := range compressedContentTypes {
-		if strings.Contains(currentContentType, compressedContentType) {
-			return w.ZipWriter.Write(b)
-		}
-	}
-	return w.CommonWriter.Write(b)
+	return w.ZipWriter.Write(b)
 }
 
 func DecompressGzipRequest(next http.Handler) http.Handler {
@@ -45,23 +28,8 @@ func DecompressGzipRequest(next http.Handler) http.Handler {
 			return
 		}
 		defer gz.Close()
-
-		unzippedBody, err := io.ReadAll(gz)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-		unzippedReader := bytes.NewBuffer(unzippedBody)
-		unzippedRequest, err := http.NewRequest(
-			r.Method,
-			r.RequestURI,
-			unzippedReader,
-		)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		unzippedRequest.Header = r.Header
-		next.ServeHTTP(w, unzippedRequest)
+		r.Body = gz
+		next.ServeHTTP(w, r)
 	})
 }
 
@@ -78,6 +46,6 @@ func CompressResponseToGzip(next http.Handler) http.Handler {
 		}
 		defer gz.Close()
 		w.Header().Set("Content-Encoding", "gzip")
-		next.ServeHTTP(gzipWriter{w, gz, w}, r)
+		next.ServeHTTP(gzipWriter{w, gz}, r)
 	})
 }
